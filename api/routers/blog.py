2dba64f072655c160e36ddd1971c219bc3d4f12e
rimusophie <rimusophie@gmail.com>
2024-04-26 17:07:31 +0900

    非同期対応

--- api/routers/blog.py
+++ api/routers/blog.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.blog as crud
 import api.schemas.blog as schema
@@ -9,13 +10,13 @@ router = APIRouter()
 
 # 一覧取得
 @router.get("/blogs", response_model = list[schema.Blog])
-async def list_blogs(db: Session = Depends(get_db)):
-    return crud.get_blogs(db)
+async def list_blogs(db: AsyncSession = Depends(get_db)):
+    return await crud.get_blogs(db)
 
 # 詳細取得
 @router.get("/blogs/{id}", response_model = schema.Blog)
-async def detail_blog(id: int, db: Session = Depends(get_db)):
-    blog = crud.get_blog(db, id = id)
+async def detail_blog(id: int, db: AsyncSession = Depends(get_db)):
+    blog = await crud.get_blog(db, id = id)
 
     if blog is None:
         raise HTTPException(status_code = 404, detail = "Blog not found")
@@ -24,25 +25,25 @@ async def detail_blog(id: int, db: Session = Depends(get_db)):
 
 # 追加
 @router.post("/blogs", response_model = schema.BlogCreateResponse)
-async def create_blog(body: schema.BlogCreate, db: Session = Depends(get_db)):
-    return crud.create_blog(db, body)
+async def create_blog(body: schema.BlogCreate, db: AsyncSession = Depends(get_db)):
+    return await crud.create_blog(db, body)
 
 # 更新
 @router.put("/blogs/{id}", response_model = schema.BlogCreateResponse)
-async def update_blog(id: int, body: schema.BlogCreate, db: Session = Depends(get_db)):
-    blog = crud.get_blog(db, id = id)
+async def update_blog(id: int, body: schema.BlogCreate, db: AsyncSession = Depends(get_db)):
+    blog = await crud.get_blog(db, id = id)
 
     if blog is None:
         raise HTTPException(status_code = 404, detail = "Blog not found")
     
-    return crud.update_blog(db, body, data=blog)
+    return await crud.update_blog(db, body, data=blog)
 
 # 削除
 @router.delete("/blogs/{id}", response_model = None)
-async def delete_blog(id: int, db: Session = Depends(get_db)):
-    blog = crud.get_blog(db, id = id)
+async def delete_blog(id: int, db: AsyncSession = Depends(get_db)):
+    blog = await crud.get_blog(db, id = id)
 
     if blog is None:
         raise HTTPException(status_code = 404, detail = "Blog not found")
 
-    return crud.delete_blog(db, data=blog)
+    return await crud.delete_blog(db, data=blog)
--- api/routers/blog_category.py
+++ api/routers/blog_category.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.blog_category as crud
 import api.schemas.blog_category as schema
@@ -9,13 +10,13 @@ router = APIRouter()
 
 # 一覧取得
 @router.get("/blog_categories", response_model = list[schema.BlogCategory])
-async def list_blog_categories(db: Session = Depends(get_db)):
-    return crud.get_blog_categories(db)
+async def list_blog_categories(db: AsyncSession = Depends(get_db)):
+    return await crud.get_blog_categories(db)
 
 # 詳細取得
 @router.get("/blog_categories/{id}", response_model = schema.BlogCategory)
-async def detail_blog_category(id: int, db: Session = Depends(get_db)):
-    blog_category = crud.get_blog_category(db, id = id)
+async def detail_blog_category(id: int, db: AsyncSession = Depends(get_db)):
+    blog_category = await crud.get_blog_category(db, id = id)
 
     if blog_category is None:
         raise HTTPException(status_code = 404, detail = "BlogCategory not found")
@@ -24,25 +25,25 @@ async def detail_blog_category(id: int, db: Session = Depends(get_db)):
 
 # 追加
 @router.post("/blog_categories", response_model = schema.BlogCategoryCreateResponse)
-async def create_blog_category(body: schema.BlogCategoryCreate, db: Session = Depends(get_db)):
-    return crud.create_blog_category(db, body)
+async def create_blog_category(body: schema.BlogCategoryCreate, db: AsyncSession = Depends(get_db)):
+    return await crud.create_blog_category(db, body)
 
 # 更新
 @router.put("/blog_categories/{id}", response_model = schema.BlogCategoryCreateResponse)
-async def update_blog_category(id: int, body: schema.BlogCategoryCreate, db: Session = Depends(get_db)):
-    blog_category = crud.get_blog_category(db, id = id)
+async def update_blog_category(id: int, body: schema.BlogCategoryCreate, db: AsyncSession = Depends(get_db)):
+    blog_category = await crud.get_blog_category(db, id = id)
 
     if blog_category is None:
         raise HTTPException(status_code = 404, detail = "BlogCategory not found")
     
-    return crud.update_blog_category(db, body, data=blog_category)
+    return await crud.update_blog_category(db, body, data=blog_category)
 
 # 削除
 @router.delete("/blog_categories/{id}", response_model = None)
-async def delete_blog_category(id: int, db: Session = Depends(get_db)):
-    blog_category = crud.get_blog_category(db, id = id)
+async def delete_blog_category(id: int, db: AsyncSession = Depends(get_db)):
+    blog_category = await crud.get_blog_category(db, id = id)
 
     if blog_category is None:
         raise HTTPException(status_code = 404, detail = "BlogCategory not found")
 
-    return crud.delete_blog_category(db, data=blog_category)
+    return await crud.delete_blog_category(db, data=blog_category)
